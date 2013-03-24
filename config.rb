eecccef5cf7d541efaa4a4de62d6feba6d597fae
Sherzod Odinaev <sherzodinaev@gmail.com>
2013-03-24 18:37:29 -0400

    initiall octopress setup

--- config.rb
+++ config.rb
@@ -4,10 +4,10 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/scrolls/"
+http_images_path = "/scrolls/images"
+http_fonts_path = "/scrolls/fonts"
+css_dir = "public/scrolls/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
